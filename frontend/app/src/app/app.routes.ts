f9bf46ece8b1ec9d2769de217879d143926a138b
Umut Yavuz <umtyavuz@icloud.com>
2026-05-03 15:27:31 +0300

    frontend: auth guard eklendi, route yapisi duzeltildi

--- frontend/app/src/app/app.routes.ts
+++ frontend/app/src/app/app.routes.ts
@@ -1,12 +1,19 @@
 import { Routes } from '@angular/router';
+import { AuthGuard } from './guards/auth-guard';
 
 export const routes: Routes = [
   {
-    path: '',
-    loadChildren: () => import('./tabs/tabs.routes').then((m) => m.routes),
+    path:'',
+    redirectTo:'login',
+    pathMatch:'full'
   },
   {
     path: 'login',
     loadComponent: () => import('./login/login.page').then( m => m.LoginPage)
   },
+  {
+    path:'tabs',
+    loadChildren:() => import ('./tabs/tabs.routes').then(m => m.routes),
+    canActivate:[AuthGuard]
+  }
 ];
